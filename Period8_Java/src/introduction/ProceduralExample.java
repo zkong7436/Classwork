818dc86314897e877ac9aa21394ad35659886fce
Student8 <Student8@10.8.33.172>
2016-09-15 13:32:48 -0400

    nom

--- Period8_Java/src/introduction/ProceduralExample.java
+++ Period8_Java/src/introduction/ProceduralExample.java
@@ -2,6 +2,6 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello, world!");
+    System.out.println("jirthnfiruhvdnihruhtioucnirhtirueghieun!");
   }
 }
