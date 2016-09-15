775e5382202bdc230e1db3c6e9190d5ca00b7178
Student8 <Student8@10.8.33.172>
2016-09-15 13:55:37 -0400

    all three says

--- Period8_Java/src/introduction/ProceduralExample.java
+++ Period8_Java/src/introduction/ProceduralExample.java
@@ -4,6 +4,8 @@ public class ProceduralExample {
   public static void main(String[] args){
 	  String[] names={"Jillian","Joseph","Jordan"};
 	  String[] years={"Senior","Junior","Senior"};
-	  System.out.println("Hi, my name is "+names[0]+"and I'm a "+years[0]);
+	  System.out.println("Hi, my name is "+names[0]+" and I'm a "+years[0]+".");
+	  System.out.println("Hi, my name is "+names[1]+" and I'm a "+years[1]+".");
+	  System.out.println("Hi, my name is "+names[2]+" and I'm a "+years[2]+".");
   }
 }
