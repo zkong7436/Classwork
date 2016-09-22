bd735a932f1d4c9fcd4da381ee80bfeb809fe063
Student8 <Student8@10.8.33.172>
2016-09-22 14:02:42 -0400

    xcv

--- Period8_Java/src/introduction/StringPractice.java
+++ Period8_Java/src/introduction/StringPractice.java
@@ -1,8 +1,54 @@
 package introduction;
 
+import java.util.Scanner;
+
 public class StringPractice {
 
+	static Scanner input;
+	static String user;
+	
 	public static void main(String[] args) {
+		//demonstrateStringMethod();
+		createFields();
+		promptName();
+		promptForever();
+	}
+	
+	public static void promptName(){
+		
+		print("Enter your name");
+		user = input.nextLine();
+		print("Glad to meet you, "+user+". For the rest of time, I will call you, "+user+". "
+				+"You may call me Computer. We should become friends.");
+	}
+	
+	public static void promptInput() {
+		
+		print("Please type something, "+user);
+		String userInput = input.nextLine();
+		print("Congratulations! you typed: "+userInput);
+	}
+	
+	public static void promptForever() {
+		while(true){
+			promptInput();
+		}
+	}
+
+	public static void createFields(){
+		
+		input = new Scanner(System.in);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void demonstrateStringMethod(){
 		String text1 = new String("Hello world");
 		String text2 = "Hello world"; //same as above
 		
@@ -19,10 +65,36 @@ public class StringPractice {
 		if(word1.compareTo(word2) < 0){
 			print( "word1 comes before word2");
 		}
+		
 	}
 	
 	public static void print(String s){
-		System.out.println(s);
+		String printString = s;
+		int cutoff = 20;
+		if(printString.length() > cutoff){
+			for(int i=0; i*cutoff < s.length(); i++){
+				printString += getCut(s, cutoff, i+1)+"\n";
+			}
+		}
+		System.out.println(printString);
 	}
-
+	
+	private static String getCut(String s, int cutoff, int cut){
+		int cutIndex = cut * cutoff;
+		if(cutIndex > s.length())cutIndex = s.length();
+		String currentCut = s.substring(0, cutIndex);
+		int indexOfLastSpace = currentCut.length()-1;
+		for(int i = currentCut.length()-1 ; i>=0; i--){
+			String letter = currentCut.substring(i, i+1);
+			if(letter.equals(" ")){
+				indexOfLastSpace = i;
+				break;
+			}
+		}
+		 
+		currentCut = currentCut.substring(0, indexOfLastSpace);
+		return currentCut;
+		
+	}
+	
 }
