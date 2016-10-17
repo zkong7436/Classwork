26ab908f060335ae15c3c1ff51daebc4bfeb9514
Student8 <Student8@10.8.33.172>
2016-10-17 14:03:44 -0400

    zcvxfv

--- Period8_Java/src/Arrays/ArraysPractice.java
+++ Period8_Java/src/Arrays/ArraysPractice.java
@@ -1,65 +1,122 @@
 package Arrays;
 
+/*
+ * primitive type[]
+ * 	"already" in the system, start as zero
+ * 	cannot mix types
+ * 
+ * Object[]
+ * 	unless initialized, start as null
+ * 	different types of objects in an array of common superclass
+ */
+
 public class ArraysPractice {
 
 	static boolean[] boos3;
 	
 	public static void main(String[] args) {
+		//how do you time a process
+		long currentTime = System.currentTimeMillis();
 		
-		//if you want to put different primitive types into an array
-		//you must use their wrapper class
-	
-		//these are two different ways to instantiate an array
-		boolean[] boos1 = new boolean[3];
-		//this can ONLY be done at the declaration
-		//because it sets size AND content
-		boolean[] boos2 = {false,false,false};
+		int x = 10;
+		increase(x);
+		System.out.println(x);
 		
-		/**this does NOT work
-		//boo3 = [false,false,true];
-			this is all that will work
-			boos3 = new boolean[3];
-		*/
+		String[] someStrings = new String[1000];
+		standardPopulate(someStrings);
+		String s = someStrings[999];
+		makeSpecial(s);
+		print(someStrings);
 		
-		/**2 ways of iterating through an array
-		STANDARD FOR LOOP
-			- the index is important to keep track of
-			- you need to customize the order
-		*/
+		//initializeArray();
+		long endTime = System.currentTimeMillis();
+		System.out.println("The process took "+(endTime - currentTime)+" ms.");
 		
-		for(int i=0; i<boos1.length; i++){
-			System.out.println(boos1[i]);
-		}
-		
-		/**
-		 * "FOR - EACH" LOOP
-		 * 		-the index is not important
-		 * 		-you don't need to customize
-		 * 		-automatically assigned a "handle"
-		 * 		-faster to write
-		 */
-		
-		for(boolean b: boos1){
-			System.out.println(b);
-		}
-		
-		//OBJECT ARRAYS
-		String[] someStrings1 = new String[3];
-		
-//		someStrings1[0] = "a";
-//		someStrings1[1] = "b";
-//		someStrings1[2] = "c";
-		
-		String[] someStrings2 = {"a","b","c"};
-		
-		for(int i = 0; i<someStrings1.length; i++){
-			someStrings1[i]= "a new String";
-		}
-		
-		//a loop to print it
-		for(String s: someStrings1){
-			System.out.println(s);
-		}
 	}
 
+	private static void increase(int i) {
+		i++;
+		
+	}
+
+	private static void makeSpecial(String s) {
+		s = "THIS STRING IS SPECIAL";
+		//this does not change the item itself
+	}
+
+	private static void standardPopulate(String[] s) {
+		/*int i = 0;
+		for(String x: s){
+			i++;
+			x = "String #"+(i+1);
+		}*/
+		
+		for(int i = 0; i < s.length; i++){
+			s[i] = "String #"+(i+1);
+		}
+		
+	}
+
+	private static void print(String[] s){
+		for(int i=0; i<s.length;i++){
+			System.out.println(s[i]);
+		}
+	}
+	
+	public static void initializeArray(){
+		//if you want to put different primitive types into an array
+				//you must use their wrapper class
+			
+				//these are two different ways to instantiate an array
+				boolean[] boos1 = new boolean[3];
+				//this can ONLY be done at the declaration
+				//because it sets size AND content
+				boolean[] boos2 = {false,false,false};
+				
+				/**this does NOT work
+				//boo3 = [false,false,true];
+					this is all that will work
+					boos3 = new boolean[3];
+				*/
+				
+				/**2 ways of iterating through an array
+				STANDARD FOR LOOP
+					- the index is important to keep track of
+					- you need to customize the order
+				*/
+				
+				for(int i=0; i<boos1.length; i++){
+					System.out.println(boos1[i]);
+				}
+				
+				/**
+				 * "FOR - EACH" LOOP
+				 * 		-the index is not important
+				 * 		-you don't need to customize
+				 * 		-automatically assigned a "handle"
+				 * 		-faster to write
+				 */
+				
+				for(boolean b: boos1){
+					System.out.println(b);
+				}
+				
+				//OBJECT ARRAYS
+				String[] someStrings1 = new String[3];
+				
+//				someStrings1[0] = "a";
+//				someStrings1[1] = "b";
+//				someStrings1[2] = "c";
+				
+				String[] someStrings2 = {"a","b","c"};
+				
+				for(int i = 0; i<someStrings1.length; i++){
+					someStrings1[i]= "a new String";
+				}
+				
+				//a loop to print it
+				for(String s: someStrings1){
+					System.out.println(s);
+				}
+	}
 }
