34b4ccda3e7dd65ea7c28a8c01d35884e5eeeec2
Student8 <Student8@10.8.33.172>
2016-10-21 14:02:17 -0400

    sdfsdf

--- Period8_Java/src/Arrays/AdvancedArrayMethods.java
+++ Period8_Java/src/Arrays/AdvancedArrayMethods.java
@@ -6,23 +6,33 @@ public class AdvancedArrayMethods {
 	
 	public static void main(String[] args) {
 		String[] array = {"a","b","c","d","e","f","g","h"};
-		swap(array,0,array.length-1);
+		shuffle(array);
+		//swap(array,0,array.length-1);
 		
-//		int[] array;
-//		array = new int[100];
-//		int[] someArray = new int[50];
-//		someArray = array;
-//		System.out.println(someArray.length);
-//		someArray[30]=1;
-//		System.out.println(array[30]);
-		
-		/*arrays = new int[100];
-		int[] someArray = new int[50];
-		methodA(someArray);*/
+
 	}
 
-	private static void swap(String[] a, int i, int j) {
-		String placeholder = a[i];
+private static void shuffle(String[] a) {
+		for(int i=0; i<a.length;i++){
+			int random = (int)(Math.random()*6);
+			swap(a, i, random);
+		}
+	}
+
+//	int[] array;
+//	array = new int[100];
+//	int[] someArray = new int[50];
+//	someArray = array;
+//	System.out.println(someArray.length);
+//	someArray[30]=1;
+//	System.out.println(array[30]);
+	
+	/*arrays = new int[100];
+	int[] someArray = new int[50];
+	methodA(someArray);*/
+	
+	private static void swap(Object[] a, int i, int j) {
+		Object placeholder = a[i];
 		a[i] = a[j];
 		a[j] = placeholder;
 		for(int e=0; e<a.length; e++){
